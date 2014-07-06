33feafd503d0a811f5c95a19cbf3443240da8b70
Freewind <pli@thoughtworks.com>
2014-07-06 21:09:02 +0800

    fix start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p logs
-nohup ./bin/scala_code_judger_api -Dhttp.port=10003 >/dev/null 2>&1 &
+nohup ./bin/scala_code_judge_api -Dhttp.port=10003 >/dev/null 2>&1 &
 # You can provide all the -Dsettings you need to set for your application here :-)
