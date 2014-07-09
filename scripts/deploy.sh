de6bc72efd68bd492387881b4c4b5925941e9472
Coney Wu <coney.wu@gmail.com>
2014-07-09 21:10:17 +0800

    enable verbose mode for deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
+#!/bin/bash +x
 
 heroku apps:destroy -a scala-code-judge-api --confirm scala-code-judge-api
 heroku apps:create scala-code-judge-api
 
 git status
 git checkout master
-git pull --rebase
 
 git status
 git add build_version
 git commit -m "update build_version file"
 
 git remote add heroku git@heroku.com:scala-code-judge-api.git
-git pull --rebase heroku master
 git status
 git push heroku master
