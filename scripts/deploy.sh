2f280e6137d5d2a525a0381a592ec3746880162a
Coney Wu <coney.wu@gmail.com>
2014-07-09 22:48:36 +0800

    parameterize project name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
-#!/bin/bash +x
+#!/bin/bash -x
 
-heroku apps:destroy -a scala-code-judge-api --confirm scala-code-judge-api
-heroku apps:create scala-code-judge-api
+PROJECT=$1
+
+heroku apps:destroy -a $PROJECT --confirm $PROJECT
+heroku apps:create $PROJECT
 
 # commit build_version in another branch
 git branch -D heroku 2>/dev/null
@@ -12,7 +14,7 @@ git add build_version -f
 git commit -m "update build_version file"
 
 # push latest code to heroku with build_version
-git remote add heroku git@heroku.com:scala-code-judge-api.git 2>/dev/null
+git remote add heroku git@heroku.com:${PROJECT}.git 2>/dev/null
 #git push heroku master
 git push -u heroku heroku:master
 
